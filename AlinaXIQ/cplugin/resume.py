9c5098b7e4119b5300ff262d9a9e0bcaa94d70b0
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-15 21:02:25 +0300

    Add files via upload

---
+++ AlinaXIQ/cplugin/resume.py
@@ -0,0 +1,101 @@
+from pyrogram import filters, Client
+from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from AlinaXIQ import app
+from AlinaXIQ.core.call import Alina
+
+from AlinaXIQ.utils.decorators import AdminRightsCheck
+from AlinaXIQ.utils.inline import close_markup
+from config import BANNED_USERS
+from AlinaXIQ import userbot
+from AlinaXIQ.core.mongo import mongodb, pymongodb
+
+authdb = mongodb.adminauth
+authuserdb = mongodb.authuser
+autoenddb = mongodb.autoend
+assdb = mongodb.assistants
+blacklist_chatdb = mongodb.blacklistChat
+blockeddb = mongodb.blockedusers
+chatsdb = mongodb.chats
+channeldb = mongodb.cplaymode
+clonebotdb = pymongodb.clonebotdb
+countdb = mongodb.upcount
+gbansdb = mongodb.gban
+langdb = mongodb.language
+onoffdb = mongodb.onoffper
+playmodedb = mongodb.playmode
+playtypedb = mongodb.playtypedb
+skipdb = mongodb.skipmode
+sudoersdb = mongodb.sudoers
+usersdb = mongodb.tgusersdb
+privatedb = mongodb.privatechats
+suggdb = mongodb.suggestion
+cleandb = mongodb.cleanmode
+queriesdb = mongodb.queries
+userdb = mongodb.userstats
+videodb = mongodb.vipvideocalls
+
+# Shifting to memory [mongo sucks often]
+active = []
+activevideo = []
+assistantdict = {}
+autoend = {}
+count = {}
+channelconnect = {}
+langm = {}
+loop = {}
+maintenance = []
+nonadmin = {}
+pause = {}
+playmode = {}
+playtype = {}
+skipmode = {}
+privatechats = {}
+cleanmode = []
+suggestion = {}
+mute = {}
+audio = {}
+video = {}
+
+
+async def is_music_playing(chat_id: int) -> bool:
+    mode = pause.get(chat_id)
+    if not mode:
+        return False
+    return mode
+
+
+async def music_on(chat_id: int):
+    pause[chat_id] = True
+
+
+async def music_off(chat_id: int):
+    pause[chat_id] = False
+
+
+@Client.on_message(
+    filters.command(["resume", "cresume","د","دەستپێکردنەوە"], prefixes=["/", "!", "%", "", ".", "@", "#"]) & ~filters.private & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def resume_com(cli, message: Message, _, chat_id):
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_3"])
+    await music_on(chat_id)
+    user_mention = message.from_user.mention if message.from_user else "𝖠𝖽𝗆𝗂𝗇"
+    await Alina.resume_stream(chat_id)
+    buttons_resume = [
+        [
+            InlineKeyboardButton(text="‣‣I 𝖲𝗄𝗂𝗉", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢ 𝖲𝗍𝗈𝗉", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text="II 𝖯𝖺𝗎𝗌𝖾",
+                callback_data=f"ADMIN Pause|{chat_id}",
+            ),
+        ],
+    ]
+    await message.reply_text(
+        _["admin_4"].format(message.from_user.mention),
+        reply_markup=InlineKeyboardMarkup(buttons_resume),
+    )
