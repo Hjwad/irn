2e2d30485f18e29665cca2cad8d276b7895e97da
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-15 13:10:55 +0300

    Update __main__.py

--- AlinaXIQ/__main__.py
+++ AlinaXIQ/__main__.py
@@ -3,7 +3,7 @@ import importlib
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
-
+from AlinaXIQ.plugins.tools.clone import restart_bots
 import config
 from AlinaXIQ import LOGGER, app, userbot
 from AlinaXIQ.core.call import Alina
@@ -49,6 +49,7 @@ async def init():
     except:
         pass
     await Alina.decorators()
+    await restart_bots()
     LOGGER("AlinaXIQ").info(
         "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
