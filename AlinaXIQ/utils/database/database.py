ec8408a21b9b75e9390339c38daab922ddf6205c
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-21 23:28:11 +0300

    Update database.py

--- AlinaXIQ/utils/database/database.py
+++ AlinaXIQ/utils/database/database.py
@@ -2,7 +2,7 @@ import random
 from typing import Dict, List, Union
 
 from AlinaXIQ import userbot
-from AlinaXIQ.core.mongo import mongodb
+from AlinaXIQ.core.mongo import mongodb, pymongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
@@ -30,6 +30,8 @@ userdb = mongodb.userstats
 videodb = mongodb.vipvideocalls
 chatsdbc = mongodb.chatsc  # for clone
 usersdbc = mongodb.tgusersdbc  # for clone
+notesdb = mongodb.notes
+welcomedb = mongodb.welcome
 
 # Shifting to memory [mongo sucks often]
 active = []
@@ -71,7 +73,93 @@ async def set_queries(mode: int):
         {"chat_id": chat_id}, {"$set": {"mode": mode}}, upsert=True
     )
 
-# Top Chats DB
+# note DB
+async def get_notes_count() -> dict:
+    chats_count = 0
+    notes_count = 0
+    async for chat in notesdb.find({"chat_id": {"$exists": 1}}):
+        notes_name = await get_note_names(chat["chat_id"])
+        notes_count += len(notes_name)
+        chats_count += 1
+    return {"chats_count": chats_count, "notes_count": notes_count}
+
+
+async def _get_notes(chat_id: int) -> Dict[str, int]:
+    _notes = await notesdb.find_one({"chat_id": chat_id})
+    if not _notes:
+        return {}
+    return _notes["notes"]
+
+
+async def get_note_names(chat_id: int) -> List[str]:
+    _notes = []
+    for note in await _get_notes(chat_id):
+        _notes.append(note)
+    return _notes
+
+
+async def get_note(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    _notes = await _get_notes(chat_id)
+    if name in _notes:
+        return _notes[name]
+    return False
+
+
+async def save_note(chat_id: int, name: str, note: dict):
+    name = name.lower().strip()
+    _notes = await _get_notes(chat_id)
+    _notes[name] = note
+
+    await notesdb.update_one(
+        {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
+    )
+
+
+async def delete_note(chat_id: int, name: str) -> bool:
+    notesd = await _get_notes(chat_id)
+    name = name.lower().strip()
+    if name in notesd:
+        del notesd[name]
+        await notesdb.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"notes": notesd}},
+            upsert=True,
+        )
+        return True
+    return False
+
+
+async def deleteall_notes(chat_id: int):
+    return await notesdb.delete_one({"chat_id": chat_id})
+####################
+
+async def get_welcome(chat_id: int) -> (str, str, str):
+    data = await welcomedb.find_one({"chat_id": chat_id})
+    if not data:
+        return "", "", ""
+
+    welcome = data.get("welcome", "")
+    raw_text = data.get("raw_text", "")
+    file_id = data.get("file_id", "")
+
+    return welcome, raw_text, file_id
+
+
+async def set_welcome(chat_id: int, welcome: str, raw_text: str, file_id: str):
+    update_data = {
+        "welcome": welcome,
+        "raw_text": raw_text,
+        "file_id": file_id,
+    }
+
+    return await welcomedb.update_one(
+        {"chat_id": chat_id}, {"$set": update_data}, upsert=True
+    )
+
+
+async def del_welcome(chat_id: int):
+    return await welcomedb.delete_one({"chat_id": chat_id})
 
 
 async def get_top_chats() -> dict:
