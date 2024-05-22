7c2b342e1e50760f6dbe0f25531501d9dfa47f22
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-22 15:14:16 +0300

    Update mantion.py

--- AlinaXIQ/plugins/admins/mantion.py
+++ AlinaXIQ/plugins/admins/mantion.py
@@ -1,16 +1,30 @@
 import asyncio
+import random
+from pyrogram import Client, filters
+from pyrogram.enums import ChatType, ChatMemberStatus
+from pyrogram.errors import UserNotParticipant
+from pyrogram.types import ChatPermissions
 from AlinaXIQ import app
-from pyrogram import filters
-from strings.filters import command
+from AlinaXIQ.utils.alina_ban import admin_filter
+
 
 SPAM_CHATS = []
 
 
-@app.on_message(command(["/mantion", "/all", "تاگ", "@all"]) & filters.group)
+@app.on_message(
+    filters.command(["all", "mention", "mentionall","تاگ"], prefixes=["/", "@", ".", ""])
+    & admin_filter
+)
 async def tag_all_users(_, message):
+    if message.chat.id in SPAM_CHATS:
+        return await message.reply_text(
+            "** پڕۆسەی تاگکردن چالاکە پێویستە ڕایبگری بە فەرمانی ➥ /cancel\nدواتر دووبارەی بکەیتەوە**"
+        )
     replied = message.reply_to_message
     if len(message.command) < 2 and not replied:
-        await message.reply_text("**ڕیپلەی نامەیە یان شتێ بکە یان لەگەڵ فەرمان نامەیە بنووسە**")
+        await message.reply_text(
+            "**ڕیپلەی نامەیە یان شتێ بکە یان لەگەڵ فەرمان نامەیە بنووسە** `@all Hi Friends`"
+        )
         return
     if replied:
         SPAM_CHATS.append(message.chat.id)
@@ -19,9 +33,9 @@ async def tag_all_users(_, message):
         async for m in app.get_chat_members(message.chat.id):
             if message.chat.id not in SPAM_CHATS:
                 break
-            usernum += 5
+            usernum += 1
             usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 1:
+            if usernum == 5:
                 await replied.reply_text(usertxt)
                 await asyncio.sleep(2)
                 usernum = 0
@@ -42,7 +56,10 @@ async def tag_all_users(_, message):
             usernum += 1
             usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
             if usernum == 5:
-                await app.send_message(message.chat.id, f'{text}\n{usertxt}')
+                await app.send_message(
+                    message.chat.id,
+                    f"{text}\n{usertxt}\n\n|| ➥ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /cancel ||",
+                )
                 await asyncio.sleep(2)
                 usernum = 0
                 usertxt = ""
@@ -52,7 +69,26 @@ async def tag_all_users(_, message):
             pass
 
 
-@app.on_message(command(["/cancel", "وەستانی تاگ"]) & ~filters.private)
+@app.on_message(
+    filters.command(
+        [
+            "stopmention",
+            "offall",
+            "cancel",
+            "وەستانی تاگ",
+            "allstop",
+            "stopall",
+            "cancelmention",
+            "offmention",
+            "mentionoff",
+            "alloff",
+            "cancelall",
+            "allcancel",
+        ],
+        prefixes=["/", "@", "#"],
+    )
+    & admin_filter
+)
 async def cancelcmd(_, message):
     chat_id = message.chat.id
     if chat_id in SPAM_CHATS:
@@ -64,4 +100,4 @@ async def cancelcmd(_, message):
 
     else:
         await message.reply_text("**پڕۆسەی تاگکردن بوونی نییە**")
-        return       
+        return
